9c09cb7fff58bde29f3bd97810b6564073427682
Kidus Zelalem <zkidu5s@gmail.com>
2023-12-20 21:36:21 +0300

    5 solved

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ * 
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
