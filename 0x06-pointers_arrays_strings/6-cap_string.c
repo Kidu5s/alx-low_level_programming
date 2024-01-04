9c019a51f92b0c7e6494761fc3e2cb89e1ceafe6
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-04 23:27:23 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,12 +3,13 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: the string
- * 
+ *
  * Return: a pointer to char
  */
 char *cap_string(char *str)
 {
-	char sep[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+	char sep[] = {',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}', ' ', '\t', '\n'};
 	int j, i;
 
 	for (i = 0; str[i]; i++)
@@ -18,7 +19,7 @@ char *cap_string(char *str)
 			if (str[0] <= 'z' && str[0] >= 'a')
 				str[0] -= 32;
 			else if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-					str[i + 1] -= 32;
+				str[i + 1] -= 32;
 		}
 	}
 	return (str);
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,10 +8,10 @@
  */
 char *leet(char *str)
 {
-	char alpha[5] = {'a', 'e','o','t','l'};
+	char alpha[5] = {'a', 'e', 'o', 't', 'l'};
 	char Alpha[5] = {'A', 'E', 'O', 'T', 'L'};
 	char num[5] = {'4', '3', '0', '7', '1'};
-	int i,j;
+	int i, j;
 
 	for (j = 0; str[j]; j++)
 	{
