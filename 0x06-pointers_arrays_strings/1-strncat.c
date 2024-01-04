f3e6e747ade2407d440a0da4201ac8058afab016
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-04 21:53:37 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i]; i++);
+	for (i = 0; dest[i]; i++)
+		;
 	for (j = 0; src[j] && j < n; j++)
 		dest[j + i] = src[j];
 	return (dest);
