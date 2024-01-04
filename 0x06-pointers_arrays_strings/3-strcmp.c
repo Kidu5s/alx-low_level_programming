10cf0199ebd55f104ba0f45c4c1f246bd500107c
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-04 22:00:07 +0300

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+
+	if (i < j)
+		return (-15);
+	else if (i == j)
+		return (0);
+	else
+		return (15);
+}
