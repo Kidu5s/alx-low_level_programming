83fa6bd40a409b08f6ed5d5f7440bb471270c585
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-04 22:24:08 +0300

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array dacay int pointer
+ * @n: number of array
+ *
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		
+}
