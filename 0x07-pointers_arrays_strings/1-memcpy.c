faed33c283f9dde3e6e9819f147de628b86e675d
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-08 22:38:48 +0300

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area
+ * @src: memory area
+ * @n: number of bytes
+ *
+ * Return: a pointer to char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i]; i++)
+		dest[i] = src[i];
+	return (dest);
+}
