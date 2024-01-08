d7a659b4c33f011f9eed2001580f5f7c8e98fc30
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-08 23:36:44 +0300

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square martix of integer
+ * @a: a pointer to int
+ * @size: the size of the matrix
+ *
+ * Return: Nothing
+ */
+void print_diasums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	int sumdiag1 = 0;
+	int sumdiag2 = 0;
+
+	while (i < (size * size))
+	{
+		sumdiag1 += a[i];
+		i += size + 1;
+	}
+	while (j < ((size * size) - size))
+	{
+		j += size - 1;
+		sumdiag2 += a[j];
+	}
+	printf("%d, %d\n", sumdiag1, sumdiag2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
+void print_diagsums(int *a, int size);
 
 
 #endif /* MAIN_H */
