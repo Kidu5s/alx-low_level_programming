a281212dfa29dc0fc8321fd7f70abf7ee50c443f
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-08 23:29:17 +0300

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ *
+ * Return: begginging of the located sub
+ * NUll if ther is not any
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (haystack != NULL)
+		return (strstr(haystack, needle));
+	else
+		return (NULL);
+}
