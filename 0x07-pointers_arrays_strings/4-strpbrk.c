b562b838905677c65cf170f2473b97e91c77e9f8
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-08 23:24:38 +0300

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strbrk - searchs a string for any of a set of bytes
+ * @s: pointer
+ * @accept: pointer
+ *
+ * Return: accept if found null if not
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	if (s != NULL)
+		return (strpbrk(s, accept));
+	else
+		return (NULL);
+}
