188a621ef757d713e763b3443d0610a62e66fcf6
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-12 21:24:15 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argv: the array
+ * @argc: the number of elelment
+ *
+ * Return: Always 0.
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
