d68fab6f4f12f4618133a5a7b39507c9e2d5756b
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-16 06:32:17 +0300

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second stirng
+ *
+ * Return: NULL if string is empty or on failure
+ * on success return a pointer to a concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len1, len2;
+	char *ptr;
+
+	for (len1 = 0; s1[len1]; len1++)
+		;
+	for (len2 = 0; s2[len2]; len2++)
+		;
+
+	ptr = malloc(sizeof(char *) * (len1 + len2 + 1));
+	if (ptr == NULL)
+		return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+		ptr[i] = s1[i];
+	for (j = 0; s2[j]; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] == '\0';
+	return (ptr);
+}
