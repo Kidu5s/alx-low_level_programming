be8f179e266fa18b409cce44233550e74ec6ca32
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-16 22:04:11 +0300

    3-alloc_grind.c

---
+++ 0x0B-malloc_free/3-alloc_grind.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocates memory for a 2D array of integers
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: a poitner to a 2D array of integers
+ * NULL on failure or withd /height is 0 or neg
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	char **ptr;
+
+	ptr = malloc(sizeof(int) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		*ptr = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			ptr(i + j) = 0;
+		}
+	}
+	return (ptr);
+}
