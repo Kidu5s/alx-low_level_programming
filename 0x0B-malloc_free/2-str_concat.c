ec768872eba5c8468e65a960cb23c6be95f9da49
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-16 21:24:11 +0300

    1-strdup.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
@@ -21,10 +25,6 @@ char *str_concat(char *s1, char *s2)
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
 	for (j = 0; s2[j]; j++)
