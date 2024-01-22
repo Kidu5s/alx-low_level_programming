23b8d26ea4472c520c8aa750c328872fb5d6061b
Kidus Zelalem <zkidu5s@gmail.com>
2024-01-22 15:04:25 +0300

    2-print_dog.c

--- 0x0D-preprocessor/0x0E-structures_typedef/2-print_dog.c
+++ 0x0D-preprocessor/0x0E-structures_typedef/2-print_dog.c
@@ -7,4 +7,15 @@
  *
  * Return: Nothing.
  */
-
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("(nil)\n");
+	if (d->age == 0)
+		printf("(nil)\n");
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	if (d == NULL)
+		printf("\n");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
