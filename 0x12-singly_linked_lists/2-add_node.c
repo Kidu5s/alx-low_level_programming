cc40e479fc7a77b5cc8659d076e9606768a118b7
Kidus Zelalem <zkidu5s@gmail.com>
2024-02-04 22:46:37 +0300

    singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+size_t _strlen(char *str);
+/**
+ * add_node - adds a new node at the beginning of a lists_t list
+ * @head: the first of the list
+ * @str: the string to be added
+ *
+ * Return: the address of the new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	char *ptr;
+	size_t len;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = strdup(str);
+	len = _strlen(ptr);
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = ptr;
+	temp->len = len;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (*head);
+	}
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
